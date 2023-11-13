be87c8d41ea9094e60e32df6b99834f59fdac0ff
puwarinnueng <43135336+puwarinnueng@users.noreply.github.com>
2023-11-13 12:47:00 +0700

    1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
+// app.module.ts
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'kM1somxcjd1@',
+      database: 'nueng12',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    TaskModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
